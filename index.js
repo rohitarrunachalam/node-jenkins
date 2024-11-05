2e18baa22a20d33629b1a46223d9cf9749efea8b
rohitarrunachalam <rrarohit@gmail.com>
2024-11-06 02:19:07 +0530

    Updated index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.get('/status', (req, res) => {
-    res.json({ message: "API is running!" });
+    res.json({ message: "API is running! and this is the change one" });
 });
 
 app.listen(PORT, () => {
